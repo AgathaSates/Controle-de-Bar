c140a382746d55d5c12743201450c34a00de37a9
Agatha Sates <satesagatha@gmail.com>
2025-06-15 04:45:51 -0300

    feat: Implementado modulo Garcom

---
+++ Controle-de-Bar.Dominio/ModuloGarcom/Garcom.cs
@@ -0,0 +1,23 @@
+﻿using Controle_de_Bar.Dominio.Compartilhado;
+
+namespace Controle_de_Bar.Dominio.ModuloGarcom;
+public class Garcom: EntidadeBase<Garcom>
+{
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+
+    public Garcom() { }
+
+    public Garcom(string nome, string cpf) : this()
+    {
+        Id = Guid.NewGuid();
+        Nome = nome;
+        Cpf = cpf;
+    }
+
+    public override void Atualizar(Garcom registroEditado)
+    {
+        Nome = registroEditado.Nome;
+        Cpf = registroEditado.Cpf;
+    }
+}
---
+++ Controle-de-Bar.Infraestrutura.Arquivos/ModuloGarcom/RepositorioGarcom.cs
@@ -0,0 +1,14 @@
+﻿using Controle_de_Bar.Dominio.ModuloGarcom;
+using Controle_de_Bar.Infraestrutura.Arquivos.Compartilhado;
+
+namespace Controle_de_Bar.Infraestrutura.Arquivos.ModuloGarcom;
+public class RepositorioGarcom : RepositorioBaseEmArquivo<Garcom>, IRepositorioGarcom
+{
+    public RepositorioGarcom(ContextoDados contexto) : base(contexto) { }
+
+    protected override List<Garcom> ObterRegistros()
+    {
+        return Contexto.Garcons;
+    }
+}
+
